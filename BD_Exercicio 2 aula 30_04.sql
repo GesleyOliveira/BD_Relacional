2c3dc1fa4232b1572c0c6a06396f0efb86a56f16
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2023-11-06 22:36:51 -0300

    att

---
+++ BD_Exercicio 2 aula 30_04.sql
@@ -0,0 +1,8 @@
+use BD_Exercicios
+
+/*1.Mostre uma lista com o código de cada filme que já foi alugado e a quantidade de vezes que foi alugado.*/
+
+select * from FILME;
+select * from LOCACOES;
+select * from CATEGORIA;
+select * from CLIENTES;
